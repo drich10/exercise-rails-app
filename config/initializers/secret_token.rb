ddf7d830ba04337d74c18f934a82acb796767829
Derek Richard <derek.richard10@gmail.com>
2014-03-06 15:03:20 -0800

    Get basic rails pieces, may reduce once unecessary parts are found

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExerciseRailsApp::Application.config.secret_key_base = 'b5caac0addce812754d1d8c7c18aa43187dc51a1356587561ec6906d5856f16c7616335a4db0ab4d100d708a17399e7b16db1d593141c69f03ee13477b7a5c4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExerciseRailsApp::Application.config.session_store :cookie_store, key: '_exercise-rails-app_session'
